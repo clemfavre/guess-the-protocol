6c0641b52b8bc81414fabdbe07023df55fbc5c8e
clemfavre <clemm.favre@gmail.com>
2025-02-24 23:54:06 +0100

    fix indentation

--- code/parser.cpp
+++ code/parser.cpp
@@ -14,31 +14,31 @@ int fsm(std::string path) {
     std::string binStr;
     char finalChar;
     while (file.get(bit)) {
-        switch(state) {
-            case 0://waiting
-              if (bit == '0') {
-                  state = 1;
-                } else if (bit == '1') {
-                  state = 0;
-                } else {
-                  if (bit!='\n') {return 1;}
-                }
-              break;
-            case 1://reading
-              binStr.clear();
-              for (int i=0; i<8; i++) {
-                if (!(bit=='0' || bit=='1')) {return 5;}
-                binStr+=bit;
-                if (!file.get(bit)) {return 4;}
-              }
-              std::bitset<8> bits(binStr);
-              unsigned char byte = bits.to_ulong();
-              finalChar = static_cast<char>(byte);
-              cout << finalChar;
-              if (bit!='0') {return 7;}
+      switch(state) {
+        case 0://waiting
+          if (bit == '0') {
+              state = 1;
+            } else if (bit == '1') {
               state = 0;
-              break;
+            } else {
+              if (bit!='\n') {return 1;}
+            }
+          break;
+        case 1://reading
+          binStr.clear();
+          for (int i=0; i<8; i++) {
+            if (!(bit=='0' || bit=='1')) {return 5;}
+            binStr+=bit;
+            if (!file.get(bit)) {return 4;}
           }
+          std::bitset<8> bits(binStr);
+          unsigned char byte = bits.to_ulong();
+          finalChar = static_cast<char>(byte);
+          cout << finalChar;
+          if (bit!='0') {return 7;}
+          state = 0;
+          break;
+        }
       }
       file.close();
       return 0;
