c4b0d2c9a9575d881bec0faf30e842681d43c2d0
clemfavre <clemm.favre@gmail.com>
2025-03-04 23:47:34 +0100

    simplified code (binStr pointer)
    no need to pass the address of binStr to readOneByte, simpler to declare binStr directly IN readOneByte

--- code/parser.cpp
+++ code/parser.cpp
@@ -7,14 +7,14 @@ enum class State{
   waiting, reading
 };
 
-int readOneByte(std::ifstream* filePtr, char* bitPtr, char* finalCharPtr, std::string* binStrPtr) {
-  binStrPtr->clear();
+int readOneByte(std::ifstream* filePtr, char* bitPtr, char* finalCharPtr) {
+  std::string binStr;
   for (int i=0; i<8; i++) {
     if (!(*bitPtr=='0' || *bitPtr=='1')) {filePtr->close(); return 5;}
-    *binStrPtr+=*bitPtr;
+    binStr+=*bitPtr;
     if (!filePtr->get(*bitPtr)) {filePtr->close(); return 4;}
   }
-  std::bitset<8> bits(*binStrPtr);
+  std::bitset<8> bits(binStr);
   unsigned char byte = bits.to_ulong();
   *finalCharPtr = static_cast<char>(byte);
   std::cout << *finalCharPtr;
@@ -28,7 +28,6 @@ int fsm(std::string path) {
   if (!file) {return 3;}
   State state = State::waiting;
   char bit;
-  std::string binStr;
   char finalChar;
   while (file.get(bit)) {
     switch(state) {
@@ -44,7 +43,7 @@ int fsm(std::string path) {
           }
         break;
       case State::reading:
-        int error = readOneByte(&file, &bit, &finalChar, &binStr);
+        int error = readOneByte(&file, &bit, &finalChar);
         if (error) {return error;}
         state = State::waiting;
         break;
